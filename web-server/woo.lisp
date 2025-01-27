15ec82f410b035ebde75c7fe8bf6a963bdbd56e1
Bruno Dias <dias.h.bruno@gmail.com>
2025-01-26 22:25:57 -0300

    rename woo conversion when transforming a woo env to request and a request to woo response.

--- web-server/woo.lisp
+++ web-server/woo.lisp
@@ -1,31 +1,25 @@
 (defpackage #:wst.routing.woo
   (:use #:cl)
-  (:import-from #:wst.routing
-                #:parse-uri
-                #:response-content
-                #:response-headers
-                #:response-status
-                #:make-request)
   (:export
-   #:woo-env->request
-   #:response->woo-response))
+   #:request-from-woo-env
+   #:response-to-woo-response))
 
 (in-package :wst.routing.woo)
 
-(defun woo-env->request (env)
+(defun request-from-woo-env (env)
   (multiple-value-bind (path query hash)
-      (parse-uri (getf env :request-uri))
-    (make-request :uri path
-                  :query query
-                  :hash hash
-                  :headers (getf env :headers)
-                  :method (getf env :request-method)
-                  :content-type (getf env :content-type)
-                  :content-length (or (getf env :content-length) 0)
-                  :content (getf env :raw-body)
-                  :data (list :env env))))
+      (wst.routing:parse-uri (getf env :request-uri))
+    (wst.routing:make-request :uri path
+                              :query query
+                              :hash hash
+                              :headers (getf env :headers)
+                              :method (getf env :request-method)
+                              :content-type (getf env :content-type)
+                              :content-length (or (getf env :content-length) 0)
+                              :content (getf env :raw-body)
+                              :data (list :env env))))
 
-(defun response->woo-response (response)
-  (list (response-status response)
-        (response-headers response)
-        (list (response-content response))))
+(defun response-to-woo-response (response)
+  (list (wst.routing:response-status response)
+        (wst.routing:response-headers response)
+        (list (wst.routing:response-content response))))
