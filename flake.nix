b1b409d07845c92c797647709f7fbfdb69e23827
Bruno Dias <dias.h.bruno@gmail.com>
2026-04-15 20:23:46 -0300

    update nix flake.

--- flake.nix
+++ flake.nix
@@ -1,28 +1,43 @@
 {
-  description = "wst flake";
+  description = "wst";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-      in {
 
-        packages.${system} = {
-          inputs = [pkgs.lispPackages.quicklisp];
-          default = pkgs.mkShell {
-            name = "wst";
-            buildInputs = [pkgs.sbcl];
+        quicklisp = pkgs.fetchurl {
+          url = "https://beta.quicklisp.org/quicklisp.lisp";
+          sha256 = "4a7a5c2aebe0716417047854267397e24a44d0cce096127411e9ce9ccfeb2c17";
+        };
+      in
+        {
+          devShells.default = pkgs.mkShell {
+            packages = [
+              pkgs.sbcl
+              pkgs.git
+            ];
+
+            shellHook = ''
+            export QUICKLISP_HOME=$PWD/.quicklisp
+
+            if [ ! -d "$QUICKLISP_HOME" ]; then
+              echo "Installing Quicklisp locally..."
+              sbcl --non-interactive \
+                --load ${quicklisp} \
+                --eval "(quicklisp-quickstart:install :path \"$QUICKLISP_HOME\")" \
+                --eval "(ql:add-to-init-file)" \
+                --quit
+            fi
+
+            echo "SBCL + Quicklisp ready"
+            echo "Run: sbcl"
+          '';
           };
-        };
-
-        devShell = pkgs.mkShell {
-          inputs = [pkgs.lispPackages.quicklisp];
-            name = "wst";
-            buildInputs = [pkgs.sbcl];
-        };
-      });
+        });
 }
