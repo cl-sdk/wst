bc8dd52a145f0dd497f69e6619bc67276296d172
Bruno Dias <dias.h.bruno@gmail.com>
2026-04-17 21:36:24 -0300

    Apply suggestions from code review
    Co-authored-by: Bruno Dias <dias.h.bruno@gmail.com>

--- request-content/package.lisp
+++ request-content/package.lisp
@@ -87,6 +87,7 @@ a string).
 
 ENCODING is the external-format keyword used when decoding a binary stream
 \(e.g. :us-ascii, :utf-8).  Defaults to :us-ascii."
+  (check-type contentstring)
   (cond
     ((streamp content)
      (let ((stream (if (subtypep (stream-element-type content) 'character)
